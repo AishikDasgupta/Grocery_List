2e20d38943c14e11020132881730f4b79cdce27e
Aishik Dasgupta <83045121+AishikDasgupta@users.noreply.github.com>
2023-09-08 19:29:56 +0530

    Add files via upload

---
+++ grocerylist.js
@@ -0,0 +1,34 @@
+$(function() {
+    // Copyright © 2023 by Aishik Dasgupta
+
+    var callback = function(event) {
+        event.preventDefault();
+        var input = $('input[type=text][name=item]'),
+            value = input.val(),
+            need = ($(event.target).attr('id') === 'addNeed'),
+            item = $('<li><input type="checkbox" name="item"> ' + value + ' <a href="#">remove</a></li>'),
+            list = (need) ? $('ul').first() : $('ul').last();
+        
+        input.val("");
+        input.focus();
+
+        if (value === "") return;
+
+        if (!need) {
+            item.find('input').prop('checked', true); // Use prop instead of attr
+        }
+        item.appendTo(list);
+    }
+
+    $('#addHave, #addNeed').click(callback);
+    
+    $('ul').on('click', 'li a', function(event){
+        $(event.target).parent('li').remove();
+    });
+
+    $('ul').on('click', 'input[type=checkbox]', function(event){
+        var listItem = $(event.target).parent('li'),
+            list = (event.target.checked) ? $('ul').last() : $('ul').first();
+        listItem.appendTo(list);
+    });
+});
